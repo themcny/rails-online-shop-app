0c0ed53edd94f549cde8938dabd3fffda8bda4e3
Nicole Yee <n.lindsey.yee@gmail.com>
2015-12-29 12:14:11 -0800

    Fix the location and category validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,8 @@ class Product < ActiveRecord::Base
   # \A[a-zA-Z]+\z
   validates :title, format: { with: /\A[a-zA-Z\s]+\z/,
                               message: "only allows letters" }
-  validates :location, :category, format: { with: /[A-Za-z0-9,\. ]{3,50}/,
+  # [A-Za-z0-9,\. ]{3,50}
+  validates :location, :category, format: { with: /\A[\D]+/,
                                             message: "only allows letters, spaces, and punctuation." }
   validates :price, :quantity, numericality: { only_integer: true }
   validates :body, length: { in: 20..1000 }
