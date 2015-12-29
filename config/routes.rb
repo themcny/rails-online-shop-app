a2dd7082d2c5f072d90900c481006631521884dd
Paul & Nicole Yee <pair+gastongouron.themcny@devbootcamp.com>
2015-12-28 17:55:23 -0800

    Add welcome view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'welcome/index'
+
   get 'products/edit'
 
   get 'products/show'
@@ -13,7 +15,7 @@ Rails.application.routes.draw do
 
   resources :products
 
-  root 'products#index'
+  root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
