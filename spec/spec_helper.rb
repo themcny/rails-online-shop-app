96efb926717d0064d307662bdf86fe67960a1295
gastongouron <gastongourogmail.l.com>
2015-12-29 11:03:31 -0800

    Fix RSPEC folders to run spec independently

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -94,39 +94,3 @@ RSpec.configure do |config|
 =end
 end
 
-# Custom tests
-require "rails_helper"
-
-# Controller product creation test
-RSpec.describe Product, :type => :model do
-  context "A new product" do
-      product = Product.create!(
-        title: "Hello",
-        body: "World",
-        location: "Everywhere",
-        category: "Web development leitmotiv",
-        price: 999,
-        quantity: 1
-        )
-
-    it "have all the fields filled up when created" do
-      expect(product != nil).to be(true)
-    end
-
-    it "have a string data types for title, body, location and category" do
-      expect(
-        (product.title).is_a?(String)    &&
-        (product.body).is_a?(String)     &&
-        (product.location).is_a?(String) &&
-        (product.category).is_a?(String)
-        ).to be(true)
-    end
-
-    it "have a integer different kind of data types for price and quantity" do
-      expect((product.price).is_a?(Integer)).to be(true)
-    end
-
-  end
-end
-
-#Models
