d6bc833242424817ef4aece5c6a3440a4afbbe32
Nicole Yee <n.lindsey.yee@gmail.com>
2015-12-29 11:16:27 -0800

    Add length validation for body

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,5 @@ class Product < ActiveRecord::Base
   validates :location, :category, format: { with: /[A-Za-z0-9,\. ]{3,50}/,
                                             message: "only allows letters, spaces, and punctuation." }
   validates :price, :quantity, numericality: { only_integer: true }
+  validates :body, length: { in: 20..1000 }
 end
