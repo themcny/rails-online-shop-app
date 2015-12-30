6e04ff875dc3ba56a25b28d570adc93b826b39ad
Nicole Yee <n.lindsey.yee@gmail.com>
2015-12-29 18:43:00 -0800

    Add current user and sessions helpers/module

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,20 @@ class SessionsController < ApplicationController
 
   end
 
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      redirect_to products_path
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def sessions_params
+    params.require(:session).permit(:email, :password)
+  end
 end
