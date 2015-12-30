bab042f0cfbd2310aedee97af71b36b6d2a9b8da
gastongouron <gastongourogmail.l.com>
2015-12-29 17:07:12 -0800

    Add breaky sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-validates :email,  presence: true, uniqueness: true
-validates :password, presence: true, length: { in: 6..20}
-validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+  validates :email,  presence: true, uniqueness: true, length: {maximum: 255}
+  validates :password, presence: true, length: { in: 6..20}
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+  has_secure_password
 end
