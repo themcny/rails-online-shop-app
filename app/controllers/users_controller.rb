c242030a021c6221268812d7ffdd168d3d132b97
Nicole Yee <n.lindsey.yee@gmail.com>
2015-12-29 18:12:08 -0800

    Add login/logout/register routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,45 +12,28 @@ class UsersController < ApplicationController
                        "UTC+04:00", "UTC+05:00", "UTC+06:00", "UTC+07:00",
                        "UTC+08:00", "UTC+09:00", "UTC+10:00", "UTC+11:00",
                        "UTC+12:00", "UTC+13:00", "UTC+14:00"]
-
-    @timezones = {
-    "UTC-12:00" => -12,
-    "UTC-11:00" => -11,
-    "UTC-10:00" => -10,
-    "UTC-09:00" => -9,
-    "UTC-08:00" => -8,
-    "UTC-07:00" => -7,
-    "UTC-06:00" => -6,
-    "UTC-05:00" => -5,
-    "UTC-04:00" => -4,
-    "UTC-03:00" => -3,
-    "UTC-02:00" => -2,
-    "UTC-01:00" => -1,
-    "UTC-00:00" => 0,
-    "UTC+01:00" => 1,
-    "UTC+02:00" => 2,
-    "UTC+03:00" => 3,
-    "UTC+04:00" => 4,
-    "UTC+05:00" => 5,
-    "UTC+06:00" => 6,
-    "UTC+07:00" => 7,
-    "UTC+08:00" => 8,
-    "UTC+09:00" => 9,
-    "UTC+10:00" =>10,
-    "UTC+11:00" =>11,
-    "UTC+12:00" =>12,
-    "UTC+13:00" =>13,
-    "UTC+14:00" =>14,
-    }
     @user = User.new
   end
 
   def create
-    @user = User.new(params[:user])    # Not the final implementation!
+    @timezone_array = ["UTC-12:00", "UTC-11:00", "UTC-10:00", "UTC-09:00",
+                       "UTC-08:00", "UTC-07:00", "UTC-06:00", "UTC-05:00",
+                       "UTC-04:00", "UTC-03:00", "UTC-02:00", "UTC-01:00",
+                       "UTC-00:00", "UTC+01:00", "UTC+02:00", "UTC+03:00",
+                       "UTC+04:00", "UTC+05:00", "UTC+06:00", "UTC+07:00",
+                       "UTC+08:00", "UTC+09:00", "UTC+10:00", "UTC+11:00",
+                       "UTC+12:00", "UTC+13:00", "UTC+14:00"]
+    @user = User.new(user_params)    # Not the final implementation!
     if @user.save
-      # Handle a successful save.
+      redirect_to users_path
     else
       render 'new'
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,41 @@ def display_error(field)
   end
 end
 
+def display_user_error(field)
+  if @user.errors[field].any?
+    "#{field.capitalize} #{@user.errors[field].first}"
+  end
+end
+
+def time_zone_conversion(time_zone)
+  @timezones_hash = {
+      "UTC-12:00" => -12,
+      "UTC-11:00" => -11,
+      "UTC-10:00" => -10,
+      "UTC-09:00" => -9,
+      "UTC-08:00" => -8,
+      "UTC-07:00" => -7,
+      "UTC-06:00" => -6,
+      "UTC-05:00" => -5,
+      "UTC-04:00" => -4,
+      "UTC-03:00" => -3,
+      "UTC-02:00" => -2,
+      "UTC-01:00" => -1,
+      "UTC-00:00" => 0,
+      "UTC+01:00" => 1,
+      "UTC+02:00" => 2,
+      "UTC+03:00" => 3,
+      "UTC+04:00" => 4,
+      "UTC+05:00" => 5,
+      "UTC+06:00" => 6,
+      "UTC+07:00" => 7,
+      "UTC+08:00" => 8,
+      "UTC+09:00" => 9,
+      "UTC+10:00" =>10,
+      "UTC+11:00" =>11,
+      "UTC+12:00" =>12,
+      "UTC+13:00" =>13,
+      "UTC+14:00" =>14,
+      }
+end
 end
