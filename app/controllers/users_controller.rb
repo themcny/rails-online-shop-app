5f17524e113b8ad05766b05fff92752e3e7693de
Nicole Yee <n.lindsey.yee@gmail.com>
2015-12-29 17:40:33 -0800

    Work on user registration:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,39 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find(params[:id])
+    @users = User.find(params[:id])
   end
 
   def new
+    @timezones = {
+      "UTC-12:00" => -12,
+      "UTC-11:00" => -11,
+      "UTC-10:00" => -10,
+      "UTC-09:00" => -9,
+      "UTC-08:00" => -8,
+      "UTC-07:00" => -7,
+      "UTC-06:00" => -6,
+      "UTC-05:00" => -5,
+      "UTC-04:00" => -4,
+      "UTC-03:00" => -3,
+      "UTC-02:00" => -2,
+      "UTC-01:00" => -1,
+      "UTC-00:00" => 0,
+      "UTC+01:00" => 1,
+      "UTC+02:00" => 2,
+      "UTC+03:00" => 3,
+      "UTC+04:00" => 4,
+      "UTC+05:00" => 5,
+      "UTC+06:00" => 6,
+      "UTC+07:00" => 7,
+      "UTC+08:00" => 8,
+      "UTC+09:00" => 9,
+      "UTC+10:00" =>10,
+      "UTC+11:00" =>11,
+      "UTC+12:00" =>12,
+      "UTC+13:00" =>13,
+      "UTC+14:00" =>14,
+    }
     @user = User.new
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
   get 'products/index'
 
   resources :products
+  resources :users
 
   root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
