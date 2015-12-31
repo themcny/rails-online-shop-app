ca351742c5964fc10e953ec539c04041fc667662
Nicole Yee <n.lindsey.yee@gmail.com>
2015-12-31 08:19:16 -0800

    Add login errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,14 @@
 module ApplicationHelper
 
-  def display_error(field)
-    if @product.errors[field].any?
-      "#{field.capitalize} #{@product.errors[field].first}"
-    end
-  end
-
-
-  def display_user_error(field)
-    if @user.errors[field].any?
-      "#{field.capitalize} #{@user.errors[field].first}"
+  def display_error(object, field)
+    if object.errors[field].any?
+      "#{field.capitalize} #{object.errors[field].first}"
     end
   end
 
   def render_errors_conditionally(object, field)
     unless object.errors.messages[field].empty?
-      render :partial => 'display_error', locals: { label: field }
+      render :partial => 'display_error', locals: { object: object, label: field }
     end
   end
 
